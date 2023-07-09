8eaece8a101859acefcc218ffd486e3bbf25dc07
MarcinWypior <marcinwypior1990@gmail.com>
2023-07-09 22:36:57 +0200

    adding buttons functionality

--- venv/ui.py
+++ venv/ui.py
@@ -3,23 +3,18 @@ from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
+
 class QuizInterface:
-    def __init__(self,quiz_brain: QuizBrain):
+    def __init__(self, quiz_brain: QuizBrain):
         self.quiz = quiz_brain
-        def click_true():
-            print("true button clicked")
-
-        def click_false():
-            print("false button clicked")
-
         self.window = Tk()
         self.window.config(padx=50, pady=50, bg=THEME_COLOR)
         self.window.title("Quizzler")
 
-        self.label = Label(text="score: 0",fg="white",bg=THEME_COLOR,padx=20,pady=20)
-        self.label.grid(row=0,column=1)
+        self.label = Label(text=f"score: {self.quiz.score}", fg="white", bg=THEME_COLOR, padx=20, pady=20)
+        self.label.grid(row=0, column=1)
 
-        self.canvas = Canvas(width=300, height=250,bg="white")
+        self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(
             150,
             100,
@@ -27,20 +22,43 @@ class QuizInterface:
             text="some question text",
             font=("Ariel", 15, "italic"),
             fill=THEME_COLOR)
-        self.canvas.grid(row=1, column=0, columnspan=2,pady=20)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
 
         true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_image,bg=THEME_COLOR, highlightthickness=0, command=click_true)
+        self.true_button = Button(image=true_image, bg=THEME_COLOR, highlightthickness=0, command=self.click_true)
         self.true_button.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_image,bg=THEME_COLOR, highlightthickness=0, command=click_false)
+        self.false_button = Button(image=false_image, bg=THEME_COLOR, highlightthickness=0, command=self.click_false)
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
 
         self.window.mainloop()
 
+    def click_true(self):
+        print("true button clicked")
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def click_false(self):
+        print("false button clicked")
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        elif not is_right:
+            self.canvas.config(bg="red")
+        self.window.after(600, self.get_next_question)
+        self.label.config(text=f"score: {self.quiz.score}")
+
+
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text,text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="no questions left")
+            self.true_button.config(state="disable")
+            self.false_button.config(state="disable")
