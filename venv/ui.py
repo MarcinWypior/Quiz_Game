dd98f07bb83c9d65bbbf5b0a80d0b6db497eeace
MarcinWypior <marcinwypior1990@gmail.com>
2023-07-09 15:50:18 +0200

    adding buttons functionality

--- venv/ui.py
+++ venv/ui.py
@@ -1,10 +1,11 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self):
-
+    def __init__(self,quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         def click_true():
             print("true button clicked")
 
@@ -20,10 +21,11 @@ class QuizInterface:
 
         self.canvas = Canvas(width=300, height=250,bg="white")
         self.question_text = self.canvas.create_text(
-            50,
-            50,
+            150,
+            100,
+            width=280,
             text="some question text",
-            font=("Ariel", 20, "italic"),
+            font=("Ariel", 15, "italic"),
             fill=THEME_COLOR)
         self.canvas.grid(row=1, column=0, columnspan=2,pady=20)
 
@@ -35,6 +37,10 @@ class QuizInterface:
         self.false_button = Button(image=false_image,bg=THEME_COLOR, highlightthickness=0, command=click_false)
         self.false_button.grid(row=2, column=1)
 
-
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text,text=q_text)
